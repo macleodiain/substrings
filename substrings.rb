9057c0a379412b8fb8d7c7a47bfc0f95a045168d
Iain Macleod <macleod@live.co.uk>
2023-07-01 19:30:48 +0100

    Updated to accept multi word strings as the input.
    Exercise complete.

--- substrings.rb
+++ substrings.rb
@@ -1,13 +1,20 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings (substring, dictionary)
+    substring_list = substring.split(' ')
+    
     results = Hash.new(0)
-    dictionary.each do |dictionary_word| 
-        if substring.include?(dictionary_word)
-            results[dictionary_word]+=1
+
+    substring_list.each do |substring_word|
+        substring_word.downcase!
+            dictionary.each do |dictionary_word| 
+              if substring_word.include?(dictionary_word)
+               results[dictionary_word]+=1
+             end
         end
     end
     puts "#{results}"
+    
 end
 
-substrings("own", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
